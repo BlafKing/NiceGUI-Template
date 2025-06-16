3df9b9382e1832b3139dc4d13f9fa72b4b9fa331
Tom <tombon59@gmail.com>
2025-06-16 02:26:24 +0200

    Update paths.py

--- backend/paths.py
+++ backend/paths.py
@@ -23,20 +23,6 @@ app_data_dir = os.path.join(user_local, project_name)
 theme_dir = os.path.join(app_data_dir, "theme")
 local_theme_css = os.path.join(theme_dir, "theme.css")
 
-# ---------------------------------------------------------- #
-
-users_dir = os.path.join(app_data_dir, "users")
-cred_file = os.path.join(users_dir, "current")
-
-token = ""
-if os.path.exists(cred_file):
-    with open(cred_file, 'r') as file:
-        token = file.read()
-
-current_user_dir = os.path.join(users_dir, token)
-config_file = os.path.join(current_user_dir, "config.json")
-playlist_dir = os.path.join(current_user_dir, "playlists")
-
 
 # ---------------------------------------------------------- #
 
@@ -48,10 +34,3 @@ if not os.path.exists(theme_dir):
 
 if not os.path.exists(local_theme_css):
     shutil.copy(static_theme_css, local_theme_css)
-
-if not os.path.exists(users_dir):
-    os.makedirs(users_dir)
-
-if token:
-    if not os.path.exists(playlist_dir):
-        os.makedirs(playlist_dir)
